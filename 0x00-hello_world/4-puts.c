0b559bb7165d02ea15dd4e90d59e1b61fa91934a
MoFoda <moxxxfoda@gmail.com>
2023-03-20 16:04:45 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints a given phrase
+ * main - Entry point
  *
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
